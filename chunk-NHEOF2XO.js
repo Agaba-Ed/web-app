fba66b9b029ced25dcc1152cb01e4a15463a2fc0
Agaba-Ed <59966248+Agaba-Ed@users.noreply.github.com>
2026-07-29 09:06:30 +0000

    Deploying to gh-pages from @ Agaba-Ed/web-app@5936f663b94695ce27b7fb823a2a0cd06f1e3a7b 🚀

---
+++ chunk-NHEOF2XO.js
@@ -0,0 +1,1 @@
+import{Ka as r}from"./chunk-BTMJVULT.js";import{ea as c,ja as e}from"./chunk-UF3NDMDQ.js";var f=(()=>{class t{constructor(){this.accountingService=e(r)}resolve(){return this.accountingService.getGlAccounts()}static{this.\u0275fac=function(i){return new(i||t)}}static{this.\u0275prov=c({token:t,factory:t.\u0275fac})}}return t})();export{f as a};
